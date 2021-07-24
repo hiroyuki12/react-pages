9888d80956aa5c2402f2ddf8f25a837d19bc4db3
hiroyuki12 <hiroyuki12@gmail.com>
2021-07-24 17:57:33 +0900

    Update Test.js.

--- src/page/auth/Test.js
+++ src/page/auth/Test.js
@@ -7,13 +7,9 @@ class Test extends Component {
         return (
           <Navbar bg="dark" variant="dark">
             <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href='/react-pages/qiita'>qiita</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <div>
-                <Button> ボタンだよ </Button>
-              </div>
+              <Nav.Link href='/react-pages'>Home</Nav.Link>
+              <Nav.Link href='/react-pages/qiita'>Qiita</Nav.Link>
+              <Nav.Link href='/react-pages/blog'>Blog</Nav.Link>
             </Nav>
           </Navbar >
         );
