7dafdd91a8ed331123488b5c47083bd413fb7478
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-09 11:05:33 +0900

    Update function.

--- src/page/Main.js
+++ src/page/Main.js
@@ -4,62 +4,60 @@ import logo from '../logo.svg';
 import '../App.css';
 import { Navbar, Nav } from 'react-bootstrap';
 
-class Main extends React.Component{
-  render(){
-    return (
-      <div className="App">
-          <Navbar bg="dark" variant="dark">
-            <Nav className="mr-auto">
-              <Nav.Link href="/react-pages/">Home</Nav.Link>
-              <Nav.Link href='/react-pages/qiita'>Qiita</Nav.Link>
-              <Nav.Link href='/react-pages/blog'>Blog</Nav.Link>
-            </Nav>
-          </Navbar >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/page/Main.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <a
-            className="App-link"
-            href="https://mbp.hatenablog.com/entry/2021/07/10/211218"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            環境構築
-          </a>
-          <Link to='/react-pages/qiita'>Qiita(API)</Link>
-          <Link to='/react-pages/zenn'>Zenn(API)</Link>
-          <a
-            href='https://zenn.dev/topics/react?order=latest' 
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Zenn(React) 外部サイト
-          </a>
-          <Link to='/react-pages/mstdn'>Mstdn(API)</Link>
-          <Link to='/react-pages/stackoverflow'>StackOverFlow(API)</Link>
-          <Link to='/react-pages/teratail'>Teratail(API)</Link>
-          <a
-            href='https://qiita.com/tags/react'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Qiita(React) 外部サイト
-          </a>
-          <Link to='/react-pages/sub'>sub -)</Link>
-        </header>
-      </div>
-    )
-  } 
+function Main() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Nav className="mr-auto">
+          <Nav.Link href="/react-pages/">Home</Nav.Link>
+          <Nav.Link href='/react-pages/qiita'>Qiita</Nav.Link>
+          <Nav.Link href='/react-pages/blog'>Blog</Nav.Link>
+        </Nav>
+      </Navbar >
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/page/Main.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://mbp.hatenablog.com/entry/2021/07/10/211218"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          環境構築
+        </a>
+        <Link to='/react-pages/qiita'>Qiita(API)</Link>
+        <Link to='/react-pages/zenn'>Zenn(API)</Link>
+        <a
+          href='https://zenn.dev/topics/react?order=latest' 
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Zenn(React) 外部サイト
+        </a>
+        <Link to='/react-pages/mstdn'>Mstdn(API)</Link>
+        <Link to='/react-pages/stackoverflow'>StackOverFlow(API)</Link>
+        <Link to='/react-pages/teratail'>Teratail(API)</Link>
+        <a
+          href='https://qiita.com/tags/react'
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Qiita(React) 外部サイト
+        </a>
+        <Link to='/react-pages/sub'>sub -)</Link>
+      </header>
+    </div>
+  )
 }
 
 export default Main;
--- src/page/Sub.js
+++ src/page/Sub.js
@@ -3,41 +3,39 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 import { Navbar, Nav } from 'react-bootstrap';
 
-class Sub extends React.Component{
-  render(){
-    return (
-      <div className="App">
-          <Navbar bg="dark" variant="dark">
-            <Nav className="mr-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href='/react-pages/qiita'>Qiita</Nav.Link>
-              <Nav.Link href='/react-pages/blog'>Blog</Nav.Link>
-            </Nav>
-          </Navbar >
-        <header className="App-header">
-          <p>
-            Edit <code>src/page/Sub.js</code> and save to reload.
-          </p>
-          <Link to='/react-pages/main'> (- main</Link>
+function Sub() {
+  return (
+    <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href='/react-pages/qiita'>Qiita</Nav.Link>
+            <Nav.Link href='/react-pages/blog'>Blog</Nav.Link>
+          </Nav>
+        </Navbar >
+      <header className="App-header">
+        <p>
+          Edit <code>src/page/Sub.js</code> and save to reload.
+        </p>
+        <Link to='/react-pages/main'> (- main</Link>
 
-          <Link to='/react-pages/login'>not Firebase(login)</Link>
-          <Link to='/react-pages/signup'>not Firebase(signup)</Link>
-          <Link to='/react-pages/home'>not Firebase home(private)</Link>
-          <Link to='/react-pages/add'>not FireStore Add(private)</Link>
-          <Link to='/react-pages/index'>not FireStore Get Index(private)</Link>
+        <Link to='/react-pages/login'>not Firebase(login)</Link>
+        <Link to='/react-pages/signup'>not Firebase(signup)</Link>
+        <Link to='/react-pages/home'>not Firebase home(private)</Link>
+        <Link to='/react-pages/add'>not FireStore Add(private)</Link>
+        <Link to='/react-pages/index'>not FireStore Get Index(private)</Link>
 
-          <Link to='/react-pages/simpletodo'>simpletodo</Link>
-          <Link to='/react-pages/todolist'>todolist</Link>
-          <Link to='/react-pages/counter'>counter</Link>
-          <Link to='/react-pages/todomatic'>todomatic</Link>
-          <Link to='/react-pages/tweet'>tweet</Link>
-          <Link to='/react-pages/clock'>clock</Link>
-          <Link to='/react-pages/testnavbar'>testnavbar</Link>
-          <Link to='/react-pages/one'>one</Link>
-        </header>
-      </div>
-    )
-  } 
+        <Link to='/react-pages/simpletodo'>simpletodo</Link>
+        <Link to='/react-pages/todolist'>todolist</Link>
+        <Link to='/react-pages/counter'>counter</Link>
+        <Link to='/react-pages/todomatic'>todomatic</Link>
+        <Link to='/react-pages/tweet'>tweet</Link>
+        <Link to='/react-pages/clock'>clock</Link>
+        <Link to='/react-pages/testnavbar'>testnavbar</Link>
+        <Link to='/react-pages/one'>one</Link>
+      </header>
+    </div>
+  )
 }
 
 export default Sub;
