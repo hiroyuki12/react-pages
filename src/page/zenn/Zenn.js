4553b0ca1e53f598193b7f52c6950737d52008f3
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-12 21:30:24 +0900

    Update function. Add useEffect. Zenn.

--- src/page/zenn/Zenn.js
+++ src/page/zenn/Zenn.js
@@ -1,33 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Search from "./Search";
 import MyNavbar from "../../components/MyNavbar";
 import Footer from "../../components/Footer";
 import moment from 'moment'
 
-class Zenn extends React.Component {
+function Zenn() {
+  const [postsList, setPostsList] = useState([])
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      postsList: [],
+  useEffect(() => {
+    handleClick();
+    return () => {
     };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-    this.handleClick = this.handleClick.bind(this);
-    this.renderImageList = this.renderImageList.bind(this);
-  }
-
-  handleClick(target) {
+  const handleClick = (target: string) => {
     const url = `https://zenn-api.netlify.app/.netlify/functions/trendTech`;
     axios
       .get(url)
       .then((res) => {
-        this.setState({ postsList: this.state.postsList.concat(res.data)});
+        setPostsList(postsList.concat(res.data));
       })
       .catch(console.error);
   }
 
-  renderImageList(list) {
+  const renderImageList = (list: string) => {
     const posts = list.map((item, index) => {
       const url = 'https://zenn.dev/' + item.user.username + '/articles/' + item.slug;
       return (
@@ -40,16 +37,14 @@ class Zenn extends React.Component {
     return posts;
   }
 
-  render() {
-    return (
-      <div>
-        <MyNavbar />
-        <Search search={this.handleClick} />
-        <ul>{this.renderImageList(this.state.postsList)}</ul>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <MyNavbar />
+      <Search search={handleClick} />
+      <ul>{renderImageList(postsList)}</ul>
+      <Footer />
+    </div>
+  );
 }
 
 export default Zenn;
