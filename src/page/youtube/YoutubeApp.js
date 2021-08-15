2bc41784f64322df2875ef8f2e56e2fce977e9ae
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-16 00:52:35 +0900

    Add link Youtube.

--- src/page/youtube/YoutubeApp.js
+++ src/page/youtube/YoutubeApp.js
@@ -5,6 +5,7 @@ function YoutubeApp() {
   console.log('Hello');
   return (
     <div>
+      <a href="https://mbp.hatenablog.com/entry/2021/08/12/082102" target="_blank" rel="noreferrer">Youtube APIを使って検索＋再生</a><br />
       <p>Youtube APIのテスト</p>
       <Api />
     </div>
