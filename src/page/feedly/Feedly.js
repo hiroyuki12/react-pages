f31264c541e67e16d96724eeace13082686baa23
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-15 13:42:03 +0900

    WIP Update Feeldy.

--- src/page/feedly/Feedly.js
+++ src/page/feedly/Feedly.js
@@ -1,47 +1,37 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import axios from "axios";
 import Search from "./Search";
 import MyNavbar from "../../components/MyNavbar";
 import Footer from "../../components/Footer";
 import moment from 'moment'
 
-class Feedly extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      postsList: []
-    };
+function Feedly() {
+  const [postsList, setPostsList] = useState([])
 
-    this.handleClick = this.handleClick.bind(this);
-    this.renderImageList = this.renderImageList.bind(this);
-  }
-
-  handleClick(target) {
+  const handleClick = (target: string) => {
     const limit = 40;
     const url = `https://cloud.feedly.com/v3/streams/contents?streamId=user/` + "process.env.FEEDLY_USER_ID" + '/category/c59b3cef-0fa1-414c-8aca-dc9678aaa85f&continuation=99999999999999';
+    const url2 = 'https://qiita.com/api/v2/tags/react/items';
 
-    // Add a request interceptor
-    axios.interceptors.request.use(function (config) {
-      config.headers.Authorization =  "process.env.FEEDLY_TOKEN";
-      const token = "process.env.FEEDLY_TOKEN";
-
-      return config;
-    });
-
-    axios
-      .get(url) 
+   axios
+      .get(url, {
+        headers: {
+          Authorization: 'Bearer FEEDLY_DEVELOPER_TOKEN',
+          'Access-Control-Allow-Origin': '*',
+        }
+      })
       .then((res) => {
-        this.setState({ postsList: res.data });
+        setPostsList(postsList.concat(res.data));
       })
       .catch(console.error);
   }
 
-  renderImageList(list) {
+  const renderImageList = (list: string) => {
     const posts = list.map((item, index) => {
       return (
         <li className="item" key={index}>
 
-<img class="css-100alwu eyfquo10" src={item.user.profile_image_url} width="40" height="40" loading="lazy" />
+<img className="css-100alwu eyfquo10" src={item.user.profile_image_url} width="40" height="40" loading="lazy" />
           <a href={item.url} target="_blank" rel="noreferrer">{item.title}</a> {moment(item.created_at).fromNow()}
         </li>
       );
@@ -49,16 +39,14 @@ class Feedly extends React.Component {
     return posts;
   }
 
-  render() {
-    return (
-      <div>
-        <MyNavbar />
-        <Search search={this.handleClick} />
-        <ul>{this.renderImageList(this.state.postsList)}</ul>
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <MyNavbar />
+      <Search search={handleClick} />
+      <ul>{renderImageList(postsList)}</ul>
+      <Footer />
+    </div>
+  );
 }
 
 export default Feedly;
--- src/page/feedly/Search.js
+++ src/page/feedly/Search.js
@@ -9,10 +9,10 @@ class Search extends React.Component {
   render() {
     return (
       <div>
-        <a href="https://mbp.hatenablog.com/entry/2021/07/15/214300" target="_blank" rel="noreferrer">ReactでQiita APIから記事情報を取得して表示</a><br />
-        <a href="https://qiita.com/kei_1011/items/cdc12086347025719152" target="_blank" rel="noreferrer">React Qiita API から記事情報を取得して表示させる - Qiita</a>
-        <h2>QiitaでReactタグありの記事を表示</h2>
-        <a href="qiitaswift">Swift</a>
+        <a href="https://mbp.hatenablog.com/entry/2021/07/15/214300" target="_blank" rel="noreferrer"></a><br />
+        <a href="https://qiita.com/kei_1011/items/cdc12086347025719152" target="_blank" rel="noreferrer"></a>
+        <h2>Feedlyの記事を表示</h2>
+        <a href="qiitaswift"></a>
         <form onSubmit={this.handleSubmit}>
           <input className="siimple-btn siimple-btn--teal" type="submit" value="Search" />
         </form>
