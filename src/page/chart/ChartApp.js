df4dc0cc4c81f98fd7247561202bcf17c843bbe4
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-16 00:54:13 +0900

    Add link chart.

--- src/page/chart/ChartApp.js
+++ src/page/chart/ChartApp.js
@@ -5,6 +5,7 @@ import { PiePlot } from './PiePlot';
 const ChartApp: React.FC = () => {
   return (
     <div className="App">
+      <a href="https://mbp.hatenablog.com/entry/2021/08/11/231449" target="_blank" rel="noreferrer">Reactで棒グラフ、円グラフを作成</a><br />
       <LinePlot />
       <PiePlot />
     </div>
