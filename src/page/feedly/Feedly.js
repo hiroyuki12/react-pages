ced375c85b66cfd972984c6b12a51585b158e201
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-17 01:06:51 +0900

    Add infinite scroll Feedly.

--- src/page/feedly/Feedly.js
+++ src/page/feedly/Feedly.js
@@ -3,13 +3,39 @@ import axios from "axios";
 import Search from "./Search";
 import MyNavbar from "../../components/MyNavbar";
 import Footer from "../../components/Footer";
-import moment from 'moment'
+import lodash from 'lodash';
+import moment from 'moment';
 
 function Feedly() {
   const [postsList, setPostsList] = useState([])
   const [page, setPage] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
   const [continuation, setContinuation] = useState("9999999999999")
+
+  // 一番下に到達したら handleClickでページを更新
+  const handleScroll = lodash.throttle(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    ) {
+      return;
+    }
+
+    // 一番下に到達した時の処理
+    //if(message !== "loading...") {
+      setPage((prevCount) => prevCount + 1);
+      console.log('page count + 1');
+    //}
+
+  }, 500);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   // pageが変化した時に実行
   useEffect(() => {
@@ -19,18 +45,15 @@ function Feedly() {
   }, [page]); // Only re-run the effect if count changes
 
   const handleClick = (target: string) => {
-    const limit = 40;
 //    const url = 'https://u2r6yb4u30.execute-api.us-east-1.amazonaws.com/default/feedly?continuation=9999999999999';
     const url = 'https://u2r6yb4u30.execute-api.us-east-1.amazonaws.com/default/feedly?continuation=' + continuation;
-//    const url = 'https://u2r6yb4u30.execute-api.us-east-1.amazonaws.com/default/feedly?continuation=1629077193000';
     setIsLoading(true);
     axios
       .get(url)
       .then((res) => {
         setPostsList(postsList.concat(res.data.items));
         setIsLoading(false);
-        //console.log("res.data.items");
-        //console.log(res.data.items);
+        setContinuation(res.data.items[99].published);
       })
       .catch(console.error);
   }
@@ -42,7 +65,7 @@ function Feedly() {
       return (
         <li className="item" key={index}>
           <img src={imgsrc} width="50" height="50" loading="lazy" />
-          <a href={item.alternate[0].href} target="_blank" rel="noreferrer">{item.title}</a> {moment(date).fromNow()}  {item.published}
+          <a href={item.alternate[0].href} target="_blank" rel="noreferrer">{item.title}</a> {moment(date).fromNow()}
         </li>
       );
     });
