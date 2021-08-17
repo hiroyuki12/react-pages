41028046efc849d029fed131e55aee11bf170d2c
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-17 22:43:35 +0900

    Add infinite scroll Mstdn.

--- src/page/mstdn/Mstdn.js
+++ src/page/mstdn/Mstdn.js
@@ -2,24 +2,50 @@ import React, { useState, useEffect } from 'react';
 import Search from "./Search";
 import MyNavbar from "../../components/MyNavbar";
 import Footer from "../../components/Footer";
-import moment from 'moment'
+import lodash from 'lodash';
+import moment from 'moment';
 
 function Mstdn() {
   const [postsList, setPostsList] = useState([])
-  const [maxId, setMaxId] = useState('999999999999999999')
+  const [page, setPage] = useState(1)
   const [isLoading, setIsLoading] = useState(false)
+  const [maxId, setMaxId] = useState('999999999999999999')
 
-  // maxIdが変化した時に実行
+  // 一番下に到達したらpageを更新 -> handleClickが実行される
+  const handleScroll = lodash.throttle(() => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop !==
+      document.documentElement.offsetHeight
+    ) {
+      return;
+    }
+
+    // 一番下に到達した時の処理
+    //if(message !== "loading...") {
+      setPage((prevCount) => prevCount + 1);
+      console.log('page count + 1');
+    //}
+
+  }, 500);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  // pageが変化した時に実行
   useEffect(() => {
     //document.title = `page = ${page}, message = ${message}`;
     handleClick();
     console.log('handleClick (useEffect)');
-  }, [maxId]); // Only re-run the effect if count changes
+  }, [page]); // Only re-run the effect if count changes
 
   const handleClick = (target: string) => {
     const limit = 40;
     const url = `https://mstdn.guru/api/v1/accounts/1/statuses?max_id=` + maxId;
-    console.log(url);
     setIsLoading(true);
 
     const headers = {}
@@ -36,6 +62,7 @@ function Mstdn() {
         } else {
           setPostsList(postsList.concat(res.data));
           setIsLoading(false);
+          setMaxId(res.data[19].id);
         }
        })
   }
@@ -45,26 +72,17 @@ function Mstdn() {
       const title = item.content.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'')
       const reblog = item.reblog
       if(reblog != null) {
-        if(index == 19) {
-          console.log('Hello 19');
-          console.log(item);
-        }
         return (
           <li className="item" key={index}>
-            <img class="css-100alwu eyfquo10" src={item.reblog.account.avatar} width="40" height="40" loading="lazy" />
+            <img className="css-100alwu eyfquo10" src={item.reblog.account.avatar} width="40" height="40" loading="lazy" />
             <a href={item.url} target="_blank" rel="noreferrer">{title}</a> {moment(item.created_at).fromNow()} 繝ｪ繝励Λ繧､謨ｰ:{item.replies_count} fav謨ｰ:{item.favourites_count}
           </li>
         );
       }
       else {
-        if(index == 19) {
-          console.log('Hello 19 reblog');
-          console.log(item);
-          setMaxId(item.id);
-        }
         return (
           <li className="item" key={index}>
-            <img class="css-100alwu eyfquo10" src={item.account.avatar} width="40" height="40" loading="lazy" />
+            <img className="css-100alwu eyfquo10" src={item.account.avatar} width="40" height="40" loading="lazy" />
             <a href={item.url} target="_blank" rel="noreferrer">{title}</a> {moment(item.created_at).fromNow()} 繝ｪ繝励Λ繧､謨ｰ:{item.replies_count} fav謨ｰ:{item.favourites_count}
           </li>
         );
