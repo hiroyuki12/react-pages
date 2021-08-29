0bbb2279ec6a538d1a20a8597b93bda83efebaca
hiroyuki12 <hiroyuki12@gmail.com>
2021-08-30 00:26:03 +0900

    Update RadioButton.js.

--- src/page/radioButton/RadioButton.js
+++ src/page/radioButton/RadioButton.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function RadioButton() {
-  const [val, setVal] = React.useState('cat');
+  const [val, setVal] = React.useState('default');
+  const [answer, setAnswer] = React.useState('');
+	
   const handleChange = e => setVal(e.target.value);
+  const handleAnswer= e => setAnswer('cat');
   return (
     <>
+      <label>
+        <input
+          type="radio"
+          value="default"
+          onChange={handleChange}
+          checked={val === 'default'}
+        />
+        default 
+      </label>
       <label>
         <input
           type="radio"
@@ -26,6 +38,18 @@ function RadioButton() {
       </label>
 
       <p>selected:{val}</p>
+
+      <label>
+        <input
+          type="radio"
+          value="answer"
+          onChange={handleAnswer}
+          checked={answer === 'answer'}
+        />
+        answer
+      </label>
+
+      <p>answer:{answer}</p>
 
       <Link to='/react-pages'>/react-pagesへ</Link> 
     </>
