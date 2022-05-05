e18f4ded487e60b15de3079df04657a311ecc2d9
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-06 02:38:29 +0900

    Add Gura YouTubeApp.

--- src/page/youtube/YoutubeApp.js
+++ src/page/youtube/YoutubeApp.js
@@ -1,13 +1,193 @@
 import React from 'react'
 import Api from './Api'
+import Gura from './Gura'
 
 function YoutubeApp() {
   console.log('Hello');
   return (
     <div>
-      <a href="https://mbp.hatenablog.com/entry/2021/08/12/082102" target="_blank" rel="noreferrer">Youtube APIを使って検索＋再生</a><br />
-      <p>Youtube APIのテスト</p>
-      <Api />
+
+    Let's start shall we?<br />
+    それじゃ、始めよっか？<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=6"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Ah- welcome welcome welcome!<br />
+    えー、いらっしゃい！いらっしゃい！ようこそ私の配信へ！<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=19"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    My name, we well, we already established that right? We're on first name basis.<br />
+    私の名前？もう知ってるよね？下の名前で呼ぶよね<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=28"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    My name is Gawr Gura.<br />
+    私の名前はがうる ぐら。<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=35"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    But please feel free to call me Gura or SAME or shark..<br />
+    でも、気軽にぐら、サメ、シャークって呼んで<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=48"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+    
+    Ok,so my birthday,well you see.<br />
+    オッケー、私の誕生日、えっとね、<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=61"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    I've been around quite a bit.and don't exactry remember my birthday.<br />
+    長く生きてきたから、正確な誕生日は覚えてないんだよね。<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=68"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+
+    But I'm going ahead to decided to choose,<br />
+    でも、いつにするか決めないと...<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=73"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    how do you English?<br />
+    英語ってどうやって喋るんだっけ？<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=84"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    I got the, okay, my birthday.<br />
+    オッケー、決めたよ、私の誕生日<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=87"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    June 20th! Wu-!<br />
+    6月20日。うぉ〜<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=92"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Maybe I shouldn't have choose this,<br />
+    この日を選ばなきゃ良かったぁ〜<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=102"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    because you can see how nervous I am from may handwriting.<br />
+    私が緊張してるの文字でバレちゃう。<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=103"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    My birthday is uh, not my birthday. It is in fact the day of a popular shark movie.<br />
+    私の誕生日に決めたけど私の誕生日じゃないんだ。実は某サメ映画の公開日だよ<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=1123"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    it isn't back the day of  a popular shark movie,shark movie.<br />
+    某サメ映画の公開日　私が生まれた頃にはなかったけどね<br />
+    <iframe
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=115"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
     </div>
   );
 }
