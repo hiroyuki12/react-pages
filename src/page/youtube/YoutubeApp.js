7719525cf538a422148be79a67e9c965ce0bb233
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-06 10:46:18 +0900

    Add YoutubeApp Contents.

--- src/page/youtube/YoutubeApp.js
+++ src/page/youtube/YoutubeApp.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Api from './Api'
+import { Link } from 'react-router-dom';
 
 function YoutubeApp() {
   console.log('Hello');
   return (
     <div>
+
+    <p><Link to='/react-pages'>Back</Link></p>
 
     Let's start shall we?<br />
     それじゃ、始めよっか？<br />
@@ -212,6 +215,188 @@ function YoutubeApp() {
       width="640"
       height="360"
       src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=114"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    you probably wondering who this is.<br />
+    皆きっと　こいつが誰だか気になってるでしょ。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=127" target="015">再生</a><br />
+    <iframe
+      name="015"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=127"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Who is this.<br />
+    誰だろう？<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=131" target="016">再生</a><br />
+    <iframe
+      name="016"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=131"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    This is my long time friend, Bloop.<br />
+    彼はね、古くからの友達のブループ。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=138" target="017">再生</a><br />
+    <iframe
+      name="017"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=138"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Spell B.L.O.O.P<br />
+    スペルはB・L・O・O・P。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=142" target="018">再生</a><br />
+    <iframe
+      name="018"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=142"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Now why is he in a bubble?<br />
+    何で泡の中にいるか？<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=150" target="019">再生</a><br />
+    <iframe
+      name="019"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=150"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    lets just say its for the better for everybody here that he stays in the bubble. You know.<br />
+    彼は泡の中にいた方が皆の身のためだよ<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=153" target="020">再生</a><br />
+    <iframe
+      name="020"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=153"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    trust me I've lived with him long enough to know.<br />
+    彼とは長い付き合いだからね　よく知ってるの<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=159" target="021">再生</a><br />
+    <iframe
+      name="021"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=159"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    upon his head, you see here?<br />
+    頭の上のここ見えるかな？<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=164" target="022">再生</a><br />
+    <iframe
+      name="022"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=164"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Okay, okay<br />
+    そう　そう<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=168" target="023">再生</a><br />
+    <iframe
+      name="023"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=168"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    emergency ration can!<br />
+    非常食の缶詰！<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=173" target="024">再生</a><br />
+    <iframe
+      name="024"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=173"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    So he can't hear anything right now<br />
+    今、彼は泡で何も聞こえないけど<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=176" target="025">再生</a><br />
+    <iframe
+      name="025"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=176"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    but if push comes to shove,<br />
+    いざという時は、<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=178" target="026">再生</a><br />
+    <iframe
+      name="026"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=178"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    well his in emergency ration, so<br />
+    彼は非常食を被っているから...<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=182" target="027">再生</a><br />
+    <iframe
+      name="027"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=182"}
       frameBorder="0"
       allowFullScreen
     /><br />
