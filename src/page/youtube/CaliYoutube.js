fb0b94c4dddbe0a9f878b64885e440c49020cbaf
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-06 14:53:24 +0900

    Add GuraYoutube,CaliYoutube.

---
+++ src/page/youtube/CaliYoutube.js
@@ -0,0 +1,162 @@
+import React from 'react'
+import Api from './Api'
+import { Link } from 'react-router-dom';
+
+function CaliYoutube() {
+  console.log('Hello');
+  return (
+    <div>
+
+    <p><Link to='/react-pages'>Back</Link></p>
+
+    Bathroom...<br />
+    お手洗いへ・・・<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=1" target="001">再生</a><br />
+    <iframe
+      name="001"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=1"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Let's take a break<br />
+    少し休憩しましょ。<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=5" target="002">再生</a><br />
+    <iframe
+      name="002"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=5"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Let's see the toilet real quick. Let's take a little break.<br />
+    少しトイレを見ます。少しだけ休憩ね。<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=7" target="003">再生</a><br />
+    <iframe
+      name="003"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=7"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    What in here? This is a toilet.<br />
+    中には何があるかな？これは便器ね。<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=9" target="004">再生</a><br />
+    <iframe
+      name="004"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=9"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Dad is mad. And we don't know why?<br />
+    父は狂気。理由は何故？<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=30" target="005">再生</a><br />
+    <iframe
+      name="005"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=30"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    We gonna find out. But we will probably die.<br />
+    原因を探す。けど、きっと死ぬ。<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=33" target="006">再生</a><br />
+    <iframe
+      name="006"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=33"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    I will not rap, un less I become unhinged.<br />
+    情緒が不安定じゃなきゃ、私はラップなんてしない。<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=35" target="007">再生</a><br />
+    <iframe
+      name="007"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=35"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    Probably because my free style is,<br />
+    だって、私のフリースタイルはきっと、<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=38" target="008">再生</a><br />
+    <iframe
+      name="008"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=38"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+    secondhand embarrancing.<br />
+    傍から見たら、恥ずかしいから・・・<br />
+    <a href="https://www.youtube.com/embed/AF1iT2_i7Go?autoplay=1&start=40" target="009">再生</a><br />
+    <iframe
+      name="009"
+      id="player"
+      width="640"
+      height="360"
+      src={"https://www.youtube.com/embed/" + "AF1iT2_i7Go?start=40"}
+      frameBorder="0"
+      allowFullScreen
+    /><br />
+    <br />
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default CaliYoutube;
