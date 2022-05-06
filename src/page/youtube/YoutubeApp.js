f3050eb905c4f83a1868599f5740bc286d4de3af
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-06 09:56:38 +0900

    Add play link YouTube.

--- src/page/youtube/YoutubeApp.js
+++ src/page/youtube/YoutubeApp.js
@@ -8,7 +8,9 @@ function YoutubeApp() {
 
     Let's start shall we?<br />
     それじゃ、始めよっか？<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=6" target="001">再生</a><br />
     <iframe
+      name="001"
       id="player"
       width="640"
       height="360"
@@ -20,7 +22,9 @@ function YoutubeApp() {
 
     Ah- welcome welcome welcome!<br />
     えー、いらっしゃい！いらっしゃい！ようこそ私の配信へ！<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=19" target="002">再生</a><br />
     <iframe
+      name="002"
       id="player"
       width="640"
       height="360"
@@ -32,7 +36,9 @@ function YoutubeApp() {
 
     My name, we well, we already established that right? We're on first name basis.<br />
     私の名前？もう知ってるよね？下の名前で呼ぶよね<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=28" target="103">再生</a><br />
     <iframe
+      name="103"
       id="player"
       width="640"
       height="360"
@@ -44,7 +50,9 @@ function YoutubeApp() {
 
     My name is Gawr Gura.<br />
     私の名前はがうる ぐら。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=35" target="003">再生</a><br />
     <iframe
+      name="003"
       id="player"
       width="640"
       height="360"
@@ -56,7 +64,9 @@ function YoutubeApp() {
 
     But please feel free to call me Gura or SAME or shark.<br />
     でも、気軽にぐら、サメ、シャークって呼んで<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=48" target="004">再生</a><br />
     <iframe
+      name="004"
       id="player"
       width="640"
       height="360"
@@ -68,7 +78,9 @@ function YoutubeApp() {
     
     Ok,so my birthday, well you see.<br />
     オッケー、私の誕生日、えっとね、<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=61" target="005">再生</a><br />
     <iframe
+      name="005"
       id="player"
       width="640"
       height="360"
@@ -80,7 +92,9 @@ function YoutubeApp() {
 
     I've been around quite a bit. and don't exactry remember my birthday.<br />
     長く生きてきたから、正確な誕生日は覚えてないんだよね。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=68" target="006">再生</a><br />
     <iframe
+      name="006"
       id="player"
       width="640"
       height="360"
@@ -93,7 +107,9 @@ function YoutubeApp() {
 
     But I'm going ahead to decided to choose,<br />
     でも、いつにするか決めないと...<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=73" target="007">再生</a><br />
     <iframe
+      name="007"
       id="player"
       width="640"
       height="360"
@@ -105,7 +121,9 @@ function YoutubeApp() {
 
     how do you English?<br />
     英語ってどうやって喋るんだっけ？<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=84" target="008">再生</a><br />
     <iframe
+      name="008"
       id="player"
       width="640"
       height="360"
@@ -117,7 +135,9 @@ function YoutubeApp() {
 
     I got the, okay, my birthday.<br />
     オッケー、決めたよ、私の誕生日<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=87" target="009">再生</a><br />
     <iframe
+      name="009"
       id="player"
       width="640"
       height="360"
@@ -129,7 +149,9 @@ function YoutubeApp() {
 
     June 20th! Wu-!<br />
     6月20日。うぉ〜<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=92" target="010">再生</a><br />
     <iframe
+      name="010"
       id="player"
       width="640"
       height="360"
@@ -141,7 +163,9 @@ function YoutubeApp() {
 
     Maybe I shouldn't have choose this,<br />
     この日を選ばなきゃ良かったぁ〜<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=102" target="011">再生</a><br />
     <iframe
+      name="011"
       id="player"
       width="640"
       height="360"
@@ -153,11 +177,13 @@ function YoutubeApp() {
 
     because you can see how nervous I am from may handwriting.<br />
     私が緊張してるの文字でバレちゃう。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=105" target="012">再生</a><br />
     <iframe
+      name="012"
       id="player"
       width="640"
       height="360"
-      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=103"}
+      src={"https://www.youtube.com/embed/" + "7JbzG593dgk?start=105"}
       frameBorder="0"
       allowFullScreen
     /><br />
@@ -165,7 +191,9 @@ function YoutubeApp() {
 
     My birthday is uh, not my birthday. <br />
     私の誕生日に決めたけど私の誕生日じゃないんだ。<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=112" target="013">再生</a><br />
     <iframe
+      name="013"
       id="player"
       width="640"
       height="360"
@@ -177,7 +205,9 @@ function YoutubeApp() {
 
     it isn't back the day of  a popular shark movie,shark movie.<br />
     某サメ映画の公開日だよ　私が生まれた頃にはなかったけどね<br />
+    <a href="https://www.youtube.com/embed/7JbzG593dgk?autoplay=1&start=114" target="014">再生</a><br />
     <iframe
+      name="014"
       id="player"
       width="640"
       height="360"
