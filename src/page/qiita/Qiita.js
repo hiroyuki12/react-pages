88f4a3f8d1ccc3dc572fb62256a19993430518d1
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-09 18:49:39 +0900

    Update page 100.

--- src/page/qiita/Qiita.js
+++ src/page/qiita/Qiita.js
@@ -141,8 +141,8 @@ export function Qiita() {
         <button onClick={() => {tagButtonClick("Flutter")}}>Flutter</button>
         {tag}<br />
         page:<button onClick={() => {pageButtonClick("1")}}>__1__</button>
-        ___:<button onClick={() => {pageButtonClick("10")}}>__10__</button>
         ___:<button onClick={() => {pageButtonClick("50")}}>__50__</button>
+        ___:<button onClick={() => {pageButtonClick("100")}}>__100__</button>
         {page}
         <ul>{renderImageList(postsList)}</ul>
 
